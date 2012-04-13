e01fe9ca98405cabe76c45deb6b0b6ad44650b6e
James Wilson <jrwilson3@gmail.com>
2012-04-13 15:36:27 -0400

    Ticket #83: Equal height grids on  7-5-brick and 5-7-brick panel layouts.

--- www/sites/all/themes/lynchburg/panels/layouts/lynchburg_12/lynchburg_12_twocol_5_7_bricks/lynchburg-12-twocol-5-7-bricks.tpl.php
+++ www/sites/all/themes/lynchburg/panels/layouts/lynchburg_12/lynchburg_12_twocol_5_7_bricks/lynchburg-12-twocol-5-7-bricks.tpl.php
@@ -6,13 +6,16 @@
   </div>
   <?php endif; ?>
 
-  <div class="panel-panel grid-5">
-    <div class="inside"><?php print $content['left_above']; ?></div>
+  <?php if ($content['left_above'] || $content['rignt_above']) : ?>
+  <div class="clearfix equal-height-container">
+    <div class="panel-panel grid-5 equal-height-element">
+      <div class="inside"><?php print $content['left_above']; ?></div>
+    </div>
+    <div class="panel-panel grid-7 equal-height-element">
+      <div class="inside"><?php print $content['right_above']; ?></div>
+    </div>
   </div>
-
-  <div class="panel-panel grid-7">
-    <div class="inside"><?php print $content['right_above']; ?></div>
-  </div>
+  <?php endif; ?>
 
   <?php if ($content['middle']) : ?>
   <div class="panel-panel grid-12 lynchburg-middle">
@@ -20,13 +23,16 @@
   </div>
   <?php endif; ?>
 
-  <div class="panel-panel grid-5">
-    <div class="inside"><?php print $content['left_below']; ?></div>
+  <?php if ($content['left_below'] || $content['rignt_below']) : ?>
+  <div class="clearfix equal-height-container">
+    <div class="panel-panel grid-5 equal-height-element">
+      <div class="inside"><?php print $content['left_below']; ?></div>
+    </div>
+    <div class="panel-panel grid-7 equal-height-element">
+      <div class="inside"><?php print $content['right_below']; ?></div>
+    </div>
   </div>
-
-  <div class="panel-panel grid-7">
-    <div class="inside"><?php print $content['right_below']; ?></div>
-  </div>
+  <?php endif; ?>
 
   <?php if ($content['bottom']) : ?>
   <div class="panel-panel grid-12 lynchburg-bottom">
