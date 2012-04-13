7f41dc431093a5e49b217558f5069182cc552942
James Wilson <jrwilson3@gmail.com>
2012-04-12 21:45:25 -0400

    Ticket #83: Equal height columns shouldnt be used on full-width grid containers.

--- www/sites/all/themes/lynchburg/panels/layouts/lynchburg_12/lynchburg_12_threecol_3_6_3/lynchburg-12-threecol-3-6-3.tpl.php
+++ www/sites/all/themes/lynchburg/panels/layouts/lynchburg_12/lynchburg_12_threecol_3_6_3/lynchburg-12-threecol-3-6-3.tpl.php
@@ -1,8 +1,10 @@
 <div class="panel-display omega-grid lynchburg-12-threecol-3-6-3 equal-height-container" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-  <div class="panel-panel grid-12 lynchburg-top equal-height-element">
+  <?php if ($content['top']) : ?>
+  <div class="panel-panel grid-12 lynchburg-top">
     <div class="inside"><?php print $content['top']; ?></div>
   </div>
+  <?php endif; ?>
 
   <div class="panel-panel grid-3 lynchburg-left-sidebar equal-height-element">
     <div class="inside"><?php print $content['left']; ?></div>
@@ -12,12 +14,14 @@
     <div class="inside"><?php print $content['main']; ?></div>
   </div>
 
-  <div class="panel-panel grid-3 lynchburg-right-sidebar">
+  <div class="panel-panel grid-3 lynchburg-right-sidebar equal-height-element">
     <div class="inside"><?php print $content['right']; ?></div>
   </div>
 
-  <div class="panel-panel grid-12 lynchburg-bottom equal-height-element">
+  <?php if ($content['bottom']) : ?>
+  <div class="panel-panel grid-12 lynchburg-bottom">
     <div class="inside"><?php print $content['bottom']; ?></div>
   </div>
+  <?php endif; ?>
 
 </div>
--- www/sites/all/themes/lynchburg/panels/layouts/lynchburg_12/lynchburg_12_twocol_7_5_bricks/lynchburg-12-twocol-7-5-bricks.tpl.php
+++ www/sites/all/themes/lynchburg/panels/layouts/lynchburg_12/lynchburg_12_twocol_7_5_bricks/lynchburg-12-twocol-7-5-bricks.tpl.php
@@ -1,23 +1,37 @@
 <div class="panel-display omega-grid lynchburg-12-twocol-7-5-bricks" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel grid-12">
+
+  <?php if ($content['top']) : ?>
+  <div class="panel-panel grid-12 lynchburg-top">
     <div class="inside"><?php print $content['top']; ?></div>
   </div>
+  <?php endif; ?>
+
   <div class="panel-panel grid-7">
     <div class="inside"><?php print $content['left_above']; ?></div>
   </div>
+
   <div class="panel-panel grid-5">
     <div class="inside"><?php print $content['right_above']; ?></div>
   </div>
-  <div class="panel-panel grid-12">
+
+  <?php if ($content['middle']) : ?>
+  <div class="panel-panel grid-12 lynchburg-middle">
     <div class="inside"><?php print $content['middle']; ?></div>
   </div>
+  <?php endif; ?>
+
   <div class="panel-panel grid-7">
     <div class="inside"><?php print $content['left_below']; ?></div>
   </div>
+
   <div class="panel-panel grid-5">
     <div class="inside"><?php print $content['right_below']; ?></div>
   </div>
-  <div class="panel-panel grid-12">
+
+  <?php if ($content['bottom']) : ?>
+  <div class="panel-panel grid-12 lynchburg-bottom">
     <div class="inside"><?php print $content['bottom']; ?></div>
   </div>
+  <?php endif; ?>
+
 </div>
