5cb9982e4febb9610b508ed9eb87cbc6274c1f6c
Kris <kris@martin-bakers.com>
2012-05-16 09:07:28 -0400

    removing styles

--- www/sites/all/themes/omega/omega/panels/layouts/omega_12/omega_12_twocol_9_3/omega-12-twocol-9-3.tpl.php
+++ www/sites/all/themes/omega/omega/panels/layouts/omega_12/omega_12_twocol_9_3/omega-12-twocol-9-3.tpl.php
@@ -1,8 +1,8 @@
 <div class="panel-display omega-grid omega-12-twocol-9-3" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel grid-9 lynchburg-main">
+  <div class="panel-panel grid-9">
     <div class="inside"><?php print $content['left']; ?></div>
   </div>
-  <div class="panel-panel grid-3 lynchburg-right-sidebar equal-height-element">
+  <div class="panel-panel grid-3">
     <div class="inside"><?php print $content['right']; ?></div>
   </div>
 </div>
